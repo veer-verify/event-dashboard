d0d5f0eea138c9557b92ed8c979506eb2461f698
veer-verify <veer.verify@gmail.com>
2026-03-13 12:12:27 +0530

    fix: uncomment apiBaseUrl and add meteDataUrl in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
   name: "prod",
   production: true,
-  // apiBaseUrl: "https://prod.ivisecurity.com",
+  apiBaseUrl: "https://prod.ivisecurity.com",
   authBaseUrl: "https://prod.ivisecurity.com/userDetails",
   mqApiBaseUrl: "https://prodmq.ivisecurity.com/queueManagement",
   eventDataUrl: "https://prod.ivisecurity.com/events_data",
   guard_monitoring_url: `https://prod.ivisecurity.com/guard_monitoring`,
+  meteDataUrl: "https://prod.ivisecurity.com/metadata",
   loggingEnabled: false,
+
 };
